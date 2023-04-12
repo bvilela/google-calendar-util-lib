d52b17d811668d75e977a3f2e62c28d5d074c52f
Bruno Vilela <brunovilela2008@hotmail.com>
2023-04-11 22:12:40 -0300

    add unit tests

---
+++ app/src/test/java/br/com/bvilela/lib/utils/DateUtilsTest.java
@@ -0,0 +1,30 @@
+package br.com.bvilela.lib.utils;
+
+import com.google.api.client.util.DateTime;
+import junit.framework.Assert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+class DateUtilsTest {
+
+    @Test
+    @DisplayName("Data uma Data válida, retorna um DateTime do Google API - Cenário 1")
+    void shouldReturnDateTimeCase1() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTime dateTime = DateUtils.convertLocalDateTimeToDateTime(now);
+        Assert.assertNotNull(dateTime);
+    }
+
+    @Test
+    @DisplayName("Data uma Data válida, retorna um DateTime do Google API - Cenário 2")
+    void shouldReturnDateTimeCase2() {
+        LocalDateTime now = LocalDateTime.of(2022,10,5, 4,30,25);
+        DateTime dateTime = DateUtils.convertLocalDateTimeToDateTime(now);
+        String expected = "2022-10-05T04:30:25.000-03:00";
+        Assert.assertEquals(expected, dateTime.toString());
+    }
+
+}
